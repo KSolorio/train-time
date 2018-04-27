341554fb281c2e437fd314b1a8fd0f2a6eec8b0a
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-04-27 01:55:21 -0400

    added on click function for adding train

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,7 +7,53 @@
   storageBucket: "",
   messagingSenderId: "639070296096"
 };
+
 firebase.initializeApp(config);
 
+//shortcut for refering back to database 
 var database = firebase.database();
-console.log(database);
+
+
+//target submit button
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+  
+  //create variables for each train 
+  var trainName = $("#train-name-input").val().trim();
+  var trainDestination = $("#destination-input").val().trim();
+  var trainFrequency = $("#frequency-input").val().trim();
+    //parsing glued hour and minutes---- moment("123", "hmm").format("HH:mm")
+  var firstTrain = ""; //moment.js
+
+  //creates new train
+  var newTrain = {
+    name: trainName, 
+    destination: trainDestination,
+    frequency: trainFrequency, 
+    first: firstTrain, 
+  };
+  
+  //pushes information of new train to database
+  database.ref().push(newTrain);
+    console.log(firstTrain)
+  //clears form after adding train 
+  $("#train-name-input").val('');
+  $("#destination-input").val('');
+  $("#frequency-input").val('');
+  $("#first-train-input").val('');
+
+});
+
+
+
+  //on click copy data into webpage
+  // $("#train-table > tbody").append("<tr><td>" + trainName + "</td<td>" + trainDestination +"</td><td>" + "</td><td>" + trainFrequency +"</td></tr>");
+
+  // caluclate next arrival and minutes away 
+
+//closing tag for firebase 
+
+
+
+
+
