456d9a340885763946c4fae46edaad090ebea755
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-04-28 18:03:42 -0400

    added styling and javascript

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {  
  // Initialize Firebase
  var config = {
   apiKey: "AIzaSyCkYav9VgePY3zHQKnkrfcMD3JBUNNai_M",
@@ -22,9 +23,11 @@ $("#add-train-btn").on("click", function(event) {
   var trainName = $("#train-name-input").val().trim();
   var trainDestination = $("#destination-input").val().trim();
   var trainFrequency = $("#frequency-input").val().trim();
-    //parsing glued hour and minutes---- moment("123", "hmm").format("HH:mm")
-  var firstTrain = ""; //moment.js
-
+  var firstTrain = $("#first-train-input").val().trim();
+  if (trainName == '' || trainDestination =='' || trainFrequency == '' || firstTrain == '') {
+    alert('please fill out all inputs');
+  }
+  
   //creates new train
   var newTrain = {
     name: trainName, 
@@ -35,23 +38,66 @@ $("#add-train-btn").on("click", function(event) {
   
   //pushes information of new train to database
   database.ref().push(newTrain);
-    console.log(firstTrain)
+
   //clears form after adding train 
   $("#train-name-input").val('');
   $("#destination-input").val('');
   $("#frequency-input").val('');
   $("#first-train-input").val('');
-
+  return false;
 });
 
+ //from firebase to webpage
+ database.ref().on("child_added", function(childSnapshot, prevChildKey){
+  console.log(childSnapshot.val());
 
+  //give each train a shortcut for their info in firebase
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainFrequency = childSnapshot.val().frequency;
+  var firstTrain = childSnapshot.val().first;
 
-  //on click copy data into webpage
-  // $("#train-table > tbody").append("<tr><td>" + trainName + "</td<td>" + trainDestination +"</td><td>" + "</td><td>" + trainFrequency +"</td></tr>");
+  //moment.js time!
 
-  // caluclate next arrival and minutes away 
+  //current time: 
+  var now = moment().format("HH:mm");
+    console.log("time now " + now);
+  //prettify train arrival time
+  var trainStart = moment.unix(firstTrain).format("hh:mm");
+  //diff between the times
+  var diffTime = moment().diff(moment(firstTrain, "hh:mm A"), "m");
+    console.log("difference in time" + diffTime);
+  //mins away
+  var minsAway = diffTime % trainFrequency;
+    console.log("remainder " + minsAway);
+  //time left 
+  var timeLeft = trainFrequency - minsAway;
+    console.log("mins till train: " + minsAway);
+  // next arrival: time now + minutes away
+  var nextArrival = moment().add(timeLeft, "m");
 
-//closing tag for firebase 
+  var newTrain = moment(nextArrival).format("hh:mm A");
+  var timeAway = timeLeft;
+    
+
+  //  display all information in html from firebase
+  $("#train-table tbody").append(
+    "<tr><td>" +
+    "</td><td>" + trainName + 
+    "</td><td>" + trainDestination + 
+    "</td><td>" + trainFrequency + 
+    "</td><td>" + newTrain + 
+    "</td><td>" + timeLeft + 
+    "</td><tr>")
+
+  });
+
+  $("thead").on("click", "button", function(e) {
+    e.preventDefault();
+    $(this).parent().remove();
+});
+  
+});
 
 
 
